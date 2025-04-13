3a30323c9e6eb1180844141a74e66d3279e09221
Lathish9 <lathishrai9@gmail.com>
2025-04-14 00:40:52 +0530

    first commit

--- src/components/Highway.jsx
+++ src/components/Highway.jsx
@@ -4,17 +4,8 @@ import './Highway.css';
 export default function Highway() {
   return (
     <div className="highway">
-      {/* Side stripes */}
-      <div className="side-stripe left-stripe" />
-      <div className="side-stripe right-stripe" />
-
-      {/* Lane dividers (center + two more) */}
-      {[...Array(15)].map((_, i) => (
-        <>
-          <div key={`line1-${i}`} className="line" style={{ left: '33%', top: `${i * 60}px` }} />
-          <div key={`line2-${i}`} className="line" style={{ left: '50%', top: `${i * 60}px` }} />
-          <div key={`line3-${i}`} className="line" style={{ left: '67%', top: `${i * 60}px` }} />
-        </>
+      {[...Array(10)].map((_, i) => (
+        <div key={i} className="line" style={{ top: `${i * 60}px` }} />
       ))}
     </div>
   );
